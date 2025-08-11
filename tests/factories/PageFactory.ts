b4fcb80d30eae66dc3d9c78ea81e448c4699e959
AnnaSparrow <Shepeleva224@gmail.com>
2025-08-12 00:06:40 +0300

    added pages

---
+++ tests/factories/PageFactory.ts
@@ -0,0 +1,6 @@
+import { Browser } from "@playwright/test";
+import {HomePage} from "../pages/HomePage";
+
+export class PageFactory {
+    
+}
--- tests/pages/HomePage.ts
+++ tests/pages/HomePage.ts
@@ -1,14 +1,22 @@
 import {Page} from '@playwright/test';
+import dotenv from 'dotenv'
+dotenv.config()
+
 
 export class HomePage {
     constructor (private page: Page) {}
 
     async goToPage() {
-        await this.page.goto('https://www.onliner.by/');
+        await this.page.goto(process.env.BASE_UI_URL || '');
     }
 
     async projectNavigation() {
         await this.page.locator('text="Мобильные телефоны"').first().click(); 
+    }
+
+    async dropDowmNews() {
+        const newsLocator = this.page.locator('text="Новости"');
+        await newsLocator.hover();
     }
 
     async search(){
